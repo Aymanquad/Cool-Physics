c1597e2d31c2d09cfe0f8f9df6955196456f9160
Aymanquad <mohammedaymanquadri@gmail.com>
2024-07-18 19:56:54 +0530

    final dep. fix

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
 const cors = require("cors");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; // Use environment variable for the port
 
-const mongoUri = process.env.MONGO_URI; // Use environment variable
+const mongoUri = "mongodb+srv://mohammedaymanquadri:Ayman2004@cluster0.txcvfxs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/CoolPhysics";
 
 const Cards = require('./models/Card');
 const cardController = require('./controllers/card'); 
@@ -14,7 +14,7 @@ const theoryCardController = require('./controllers/theoryCard');
 
 // Middleware
 app.use(cors({
-  origin: ["https://cool-physics.vercel.app"], // Update this to match your Vercel deployment URL
+  origin: ["https://cool-physics-backend.vercel.app/"], // Update this to match your Vercel deployment URL
   methods: ["POST", "GET"],
   credentials: true
 }));
@@ -24,23 +24,28 @@ app.get('/', (req, res) => {
   res.json("backend habibi !");
 });
 
-app.get('/cards/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const card = await Cards.findOne({ id: id.trim() }).maxTimeMS(5000); // Example of setting a timeout of 5 seconds (5000 milliseconds)
-    
-    if (card) {
-      res.json(card);
-    } else {
-      res.status(404).json({ error: "Card not found" });
-    }
-  } catch (error) {
-    console.error("Error occurred:", error);
-    res.status(500).json({ message: 'Server error', error });
-  }
+// Route to get card by ID
+app.get('/cards/:id', (req, res) => {
+  const { id } = req.params;
+
+  Cards.findOne({ id: id.trim() })
+    .then(card => {
+      if (card) {
+        res.json(card);
+      } else {
+        res.status(404).json({ error: "Card not found" });
+      }
+    })
+    .catch(error => {
+      console.log("some err occured in finding");
+      res.status(500).json({ message: 'Server error', error });
+    });
 });
+
+// Route to get card by ID for theory/paradoxes
 app.get('/theorycards/:id', (req, res) => {
   const { id } = req.params;
+
   TheoryCards.findOne({ id: id.trim() })
     .then(theorycard => {
       if (theorycard) {
@@ -50,12 +55,12 @@ app.get('/theorycards/:id', (req, res) => {
       }
     })
     .catch(error => {
-      console.log("some err occured in finding", error);
+      console.log("some err occured in finding");
       res.status(500).json({ message: 'Server error', error });
     });
 });
 
-mongoose.connect(mongoUri)
+mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => { 
     console.log('Connected to MongoDB !'); 
     app.listen(PORT, () => {
